b3a2c13ed7be57230a27731d077bdddeeb5d3cc2
Angad Wagh <waghangad@gmail.com>
2026-04-08 20:12:39 +0530

    creating dynamic bucket name

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ data "aws_caller_identity" "current" {}
 
 module "create_s3_bucket" {
     source        = "./modules/s3"
-    bucket_name   = var.bucket_name
+    bucket_name   = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
 }
