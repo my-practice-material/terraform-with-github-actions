2b880197ca3d9216c725108edc87dfd8a653115e
Angad Wagh <waghangad@gmail.com>
2026-04-17 20:55:52 +0530

    updated admin arn

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -55,13 +55,13 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 
 resource "aws_eks_access_entry" "admin" {
   cluster_name = aws_eks_cluster.study-eks-cluster.name
-  principal_arn = var.cluster_admin_role.arn
+  principal_arn = var.cluster_admin_role_arn
   type = "STANDARD"
 }
 
 resource "aws_eks_access_policy_association" "admin" {
   cluster_name = aws_eks_cluster.study-eks-cluster.name
-  principal_arn = var.cluster_admin_role.arn
+  principal_arn = var.cluster_admin_role_arn
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   access_scope {
     type = "Cluster"
