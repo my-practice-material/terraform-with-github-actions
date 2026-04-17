626c0e87b9fb07a7ba49ed604aae3506e7df02b6
Angad Wagh <waghangad@gmail.com>
2026-04-17 18:44:15 +0530

    updated reference of cluster

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -54,11 +54,11 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = aws_eks_cluster.study-eks-cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.study-eks-cluster.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.main.token
 }
 
 data "aws_eks_cluster_auth" "main" {
-  name = module.eks.cluster_id
+  name = aws_eks_cluster.study-eks-cluster.id
 }
