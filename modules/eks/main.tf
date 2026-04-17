a0097aff49417ee84d9fc1eaab1111954e0ff604
Angad Wagh <waghangad@gmail.com>
2026-04-17 21:06:05 +0530

    updated access_scope for role

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -64,7 +64,7 @@ resource "aws_eks_access_policy_association" "admin" {
   principal_arn = var.cluster_admin_role_arn
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   access_scope {
-    type = "Cluster"
+    type = "cluster"
   }
 }
 
